313a74c5dbcbaafc4226c9a9fab9254ae53748e2
amirandavillavicencio <amirandavillavicencio@gmail.com>
2026-03-31 10:39:29 -0300

    Fix xUnit test project references and Fact usage

---
+++ AppPortable.Tests/Pipeline/PipelineTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace AppPortable.Tests.Pipeline;
+
+public class PipelineTests
+{
+    [Fact]
+    public void Placeholder_ShouldPass()
+    {
+        Assert.True(true);
+    }
+}
---
+++ AppPortable.Tests/Search/SearchTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace AppPortable.Tests.Search;
+
+public class SearchTests
+{
+    [Fact]
+    public void Placeholder_ShouldPass()
+    {
+        Assert.True(true);
+    }
+}
